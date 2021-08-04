70ad904553216399cada6e551d9599de5c9aaf63
YDeltagon <git@ydeltagon.fr>
2021-08-04 16:23:56 +0200

    V0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ListoFy
+Organize your playlist by genre / artists / moods and mark the best of them
+
+## V0.0.1
+- V0.0.1
+    - Test your Market and OAuth 
+    - Save your config in .txt files
--- main.py
+++ main.py
@@ -1,32 +1,92 @@
 ### import
-import json
 import requests
 from requests.structures import CaseInsensitiveDict
-from requests.exceptions import HTTPError
+
+
+### Spotify MARKET check
+def user_market():
+    market_check = ["FR", "EN", "GB"]
+    in_user_market = 0
+    try: 
+        f_user_market = open("user_market.txt", "r")
+        in_user_market = f_user_market.read()
+        while not in_user_market in market_check:
+            in_user_market = input("What is your market ? ")
+            if in_user_market in market_check:
+                f_save_user_market = input("Save the MARKET on a txt file ? (Y or N)")
+                if f_save_user_market == "Y":
+                    try:
+                        f_user_market = open("user_market.txt", "w")
+                        f_user_market.write(in_user_market)
+                        f_user_market.close()
+                        print("save")
+                    except:
+                        print("ERROR : The most common error is the missing file, i create one, retry !")
+                elif f_save_user_market == "N":
+                    print("not save")
+                else: 
+                    print("WTF ? (not save)")
+    except:
+        while not in_user_market in market_check:
+            in_user_market = input("What is your market ? ")
+        f_save_user_market = input("Save the MARKET on a txt file ? (Y or N)")
+        if f_save_user_market == "Y":
+            try:
+                f_user_market = open("user_market.txt", "w")
+                f_user_market.write(in_user_market)
+                f_user_market.close()
+                print("save")
+            except:
+                print("ERROR : The most common error is the missing file, i create one, retry !")
+        elif f_save_user_market == "N":
+            print("not save")
+        else: 
+            print("WTF ? (not save)")
+    return in_user_market
+        
+### Spotify OAUTH check
+def user_oauth():
+    oauth_verified = 0
+    try: 
+        f_user_oauth= open("user_oauth.txt", "r")
+        in_user_oauth = f_user_oauth.read()
+        url = "https://api.spotify.com/v1/me"
+        headers = CaseInsensitiveDict()
+        headers["Accept"] = "application/json"
+        headers["Content-Type"] = "application/json"
+        headers["Authorization"] = "Bearer " + in_user_oauth
+        if requests.get(url, headers=headers).status_code == 200:
+            print('Success !')
+            oauth_verified = 1
+        elif requests.get(url, headers=headers).status_code == 404:
+            print('Not Found.')
+        elif requests.get(url, headers=headers).status_code == 401:
+            print('Unauthorized.')
+        else:
+            print('WTF bro ?')
+    except:
+        while not oauth_verified == 1:
+            in_user_oauth = input("What is your OAuth Token ? ")
+            url = "https://api.spotify.com/v1/me"
+            headers = CaseInsensitiveDict()
+            headers["Accept"] = "application/json"
+            headers["Content-Type"] = "application/json"
+            headers["Authorization"] = "Bearer " + in_user_oauth
+            if requests.get(url, headers=headers).status_code == 200:
+                print('Success !')
+                oauth_verified = 1
+            elif requests.get(url, headers=headers).status_code == 404:
+                print('Not Found.')
+            elif requests.get(url, headers=headers).status_code == 401:
+                print('Unauthorized.')
+            else:
+                print('WTF bro ?')
+    return in_user_oauth
+
 
 ### Auth on API Spotify
-in_user_market = input("What is your market ? (FR/EN/IT...) ")
-in_user_oauth = input("What is your OAuth Token ? ")
-url = f"https://api.spotify.com/v1/me/tracks?offset=0&limit=50&market={in_user_market}"
+user_market=user_market()
+user_oauth=user_oauth()
 
-### Give like track/artist/album on Spotify
-headers = CaseInsensitiveDict()
-headers["Accept"] = "application/json"
-headers["Content-Type"] = "application/json"
-headers["Authorization"] = f"Bearer {in_user_oauth}"
-
-### 
-resp = requests.get(url, headers=headers)
-
-### print status code
-print(resp)
-
-### Succes or not
-if resp.status_code == 200:
-    print('Success !')
-elif resp.status_code == 404:
-    print('Not Found.')
-elif resp.status_code == 401:
-    print('Unauthorized.')
-else:
-    print('WTF bro ?')
+print(user_market)
+print(user_oauth)
