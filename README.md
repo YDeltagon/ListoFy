5525b86b7b34813cda5efb5ab13bd77eaa227e2a
YDeltagon <git@ydeltagon.fr>
2021-08-05 11:56:49 +0200

    V0.1.0 - Based now on [Plamere/SpotiPY]

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # ListoFy
 Organize your playlist by genre / artists / moods and mark the best of them
 
-
-## V0.0.1
+## V0.1.x
+- V0.1.0
+    - Based now on [Plamere/SpotiPY](https://github.com/plamere/spotipy) (thanks)
+    - Rewrite all my code
+    - Extract all like music on my account
+## V0.0.x
 - V0.0.2 - Last standalone version
     - Debug
     - Use the previous save for market and oauth
--- main.py
+++ main.py
@@ -1,119 +1,31 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
-#* Import
-import requests
-from requests.structures import CaseInsensitiveDict
+f_APP_CLIENT_ID = open("APP_CLIENT_ID.txt", "r")
+APP_CLIENT_ID = f_APP_CLIENT_ID.read()
+f_APP_CLIENT_SECRET = open("APP_CLIENT_SECRET.txt", "r")
+APP_CLIENT_SECRET = f_APP_CLIENT_SECRET.read()
 
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=APP_CLIENT_ID,
+                                               client_secret=APP_CLIENT_SECRET,
+                                               redirect_uri="http://localhost/",
+                                               scope="user-library-read"))
 
-#* Spotify MARKET check
-def user_market():
-    market_check = ["FR", "EN", "GB"]
-    in_user_market = 0
-    try: 
-        f_user_market = open("user_market.txt", "r")
-        in_user_market = f_user_market.read()
-        while not in_user_market in market_check:
-            in_user_market = input("What is your market ? ")
-            if in_user_market in market_check:
-                f_save_user_market = input("Save the MARKET on a txt file ? (Y or N)")
-                if f_save_user_market == "Y":
-                    try:
-                        f_user_market = open("user_market.txt", "w")
-                        f_user_market.write(in_user_market)
-                        f_user_market.close()
-                        print("save")
-                    except:
-                        print("ERROR : The most common error is the missing file, i create one, retry !")
-                elif f_save_user_market == "N":
-                    print("not save")
-                else: 
-                    print("WTF ? (not save)")
-    except:
-        while not in_user_market in market_check:
-            in_user_market = input("What is your market ? ")
-        f_save_user_market = input("Save the MARKET on a txt file ? (Y or N)")
-        if f_save_user_market == "Y":
-            try:
-                f_user_market = open("user_market.txt", "w")
-                f_user_market.write(in_user_market)
-                f_user_market.close()
-                print("save")
-            except:
-                print("ERROR : The most common error is the missing file, i create one, retry !")
-        elif f_save_user_market == "N":
-            print("not save")
-        else: 
-            print("WTF ? (not save)")
-    return in_user_market
+last_track = 0
+limit = 50
+offset = 0
+results = sp.current_user_saved_tracks()
+total_song = (results['total']-1)
+print("total : " + str(total_song))
+while not last_track == 1 :
+    results = sp.current_user_saved_tracks(limit, offset)
 
+    for idx, item in enumerate(results['items']):
+        track = item['track']
+        print(offset + idx, track['artists'][0]['name'], " – ", track['name'])
 
-#* Spotify OAUTH check
-def user_oauth():
-    oauth_verified = 0
-    if oauth_verified == 0 :
-        try: 
-            f_user_oauth = open("user_oauth.txt", "r")
-            in_user_oauth = f_user_oauth.read()
-            url = "https://api.spotify.com/v1/me"
-            headers = CaseInsensitiveDict()
-            headers["Accept"] = "application/json"
-            headers["Content-Type"] = "application/json"
-            headers["Authorization"] = "Bearer " + in_user_oauth
-            if requests.get(url, headers = headers).status_code == 200:
-                print('Success !')
-                oauth_verified = 1
-            elif requests.get(url, headers = headers).status_code == 404:
-                print('Not Found.')
-                oauth_verified = 5
-            elif requests.get(url, headers = headers).status_code == 401:
-                print('Unauthorized.')
-                oauth_verified = 5
-            else:
-                print('WTF bro ?')
-                oauth_verified = 5
-        except:
-            in_user_oauth = input("What is your OAuth Token ? ")
-            url = "https://api.spotify.com/v1/me"
-            headers = CaseInsensitiveDict()
-            headers["Accept"] = "application/json"
-            headers["Content-Type"] = "application/json"
-            headers["Authorization"] = "Bearer " + in_user_oauth
-            if requests.get(url, headers = headers).status_code == 200:
-                print('Success !')
-                oauth_verified = 1
-            elif requests.get(url, headers = headers).status_code == 404:
-                print('Not Found.')
-                oauth_verified = 5
-            elif requests.get(url, headers = headers).status_code == 401:
-                print('Unauthorized.')
-                oauth_verified = 5
-            else:
-                print('WTF bro ?')
-                oauth_verified = 5
-    while oauth_verified == 5 :
-        in_user_oauth = input("What is your OAuth Token ? ")
-        url = "https://api.spotify.com/v1/me"
-        headers = CaseInsensitiveDict()
-        headers["Accept"] = "application/json"
-        headers["Content-Type"] = "application/json"
-        headers["Authorization"] = "Bearer " + in_user_oauth
-        if requests.get(url, headers = headers).status_code == 200:
-            print('Success !')
-            oauth_verified = 1
-        elif requests.get(url, headers = headers).status_code == 404:
-            print('Not Found.')
-            oauth_verified = 5
-        elif requests.get(url, headers = headers).status_code == 401:
-            print('Unauthorized.')
-            oauth_verified = 5
-        else:
-            print('WTF bro ?')
-            oauth_verified = 5
-    return in_user_oauth
+    offset = offset + limit
+    if offset >= total_song:
+        last_track = 1
 
-
-#* Auth on API Spotify
-user_market = user_market()
-user_oauth = user_oauth()
-
-print(user_market)
-print(user_oauth)
+print("total : " + str(total_song))
